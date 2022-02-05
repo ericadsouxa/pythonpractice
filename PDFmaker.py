b222beb15cabbde40b422dfd8b342672b352478f
xxericaxx <89775076+xxericaxx@users.noreply.github.com>
2022-02-05 22:15:49 +0530

    read comments

--- PDFmaker.py
+++ PDFmaker.py
@@ -6,12 +6,22 @@
 #import module
 from fpdf import FPDF
 
+#assign FPDF module
+#variable class
 pdf= FPDF()
 
+#adds page 
 pdf.add_page()
+
+#adds font
 pdf.set_font("Arial", size = 15)
+
+#adds text(cell1)
 pdf.cell(200, 10, txt = "ERICA BASTYAV DSOUZA",
          ln = 1, align = 'C')
+#adds text(cell2)
 pdf.cell(200, 10, txt="IT ENGINEERING STUDENT.",
          ln=2, align='C')
+
+#nomenclature to save the PDF
 pdf.output("info.pdf")
